bcb037e72b14f9c1b2a8a7e78c3d54de43856897
sa-gar <67649354+sa-gar@users.noreply.github.com>
2026-05-19 18:04:01 +0530

    Fix user registration and MongoDB connection

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -135,28 +135,24 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-
-  next();
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre("save", function (next) {
+userSchema.pre("save", function () {
   if (!this.memberId && this.role === "member") {
     const randomNumber = Math.floor(100000 + Math.random() * 900000);
     this.memberId = `GYM-${randomNumber}`;
   }
-
-  next();
 });
 
 const User = mongoose.model("User", userSchema);
